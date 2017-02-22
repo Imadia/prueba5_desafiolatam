590e798b2ab657ab605af8e2ce734c7a431db6c4
Alejandra Diaz <ygthar@gmail.com>
2017-02-22 11:44:33 -0300

    Scopes agregados en Item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user, optional: true
-  validates :serial, uniqueness: true
+  #validates :serial, uniqueness: true
+  scope :filter_size, -> (size) { where("size=?", size)}
+  scope :unassigned, -> { where(user_id: nil) }
+
+ 
 
 end
