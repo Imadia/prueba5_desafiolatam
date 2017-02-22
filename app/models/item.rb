20bfd75decf768b84cd5521d74b592d34d12e1f5
Alejandra Diaz <ygthar@gmail.com>
2017-02-22 11:50:42 -0300

    borrado en cascada, borrar categorias vacias

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,15 @@ class Item < ApplicationRecord
   #validates :serial, uniqueness: true
   scope :filter_size, -> (size) { where("size=?", size)}
   scope :unassigned, -> { where(user_id: nil) }
+  after_destroy :delete_categories
+
+	def delete_categories
+	 	Category.all.each do |category|
+	 		unless category.items.any?
+	 			category.destroy
+	 		end
+	 	end
+	end
 
  
 
