58c4515a0e30c910fa3c9a141c6aa2403a9f6ff4
Alejandra Diaz <ygthar@gmail.com>
2017-02-22 13:50:30 -0300

    last commit

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,11 +1,12 @@
 class WorksController < ApplicationController
   def index
   	@item = Item.all
-
 	  	if params[:delete].present?
-	  		i = params[:delete]
-	  		elsif Item.find_by(serial: i)
-	  			b = Item.find_by(serial: i)
+	  		@i = params[:delete]
+	  	end
+
+	  	if Item.find_by(serial: @i).present?
+	  			b = Item.find_by(serial: @i)
 	  			b.destroy
 	  			redirect_to works_index_path, notice: "El producto #{params[:delete]} ha sido eliminado"
 		end
